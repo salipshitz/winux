74584a82b1fb1118466aba53c677a3fd7cb10d1b
Sa'ar <geger40.1@gmail.com>
2018-08-28 07:50:57 -0700

    Add/Implement split_args method
    split_args replaces the original manual implementation of split with the function

--- winux.py
+++ winux.py
@@ -1,7 +1,49 @@
 import os
 
+def split_args(ui):
+    i = 0
+    j = 0
+    sep = None
+    optnl = False
+    cmd = ""
+    args = []
+    opt_args = []
+    tmp
+    def done_split:
+        j = 0
+        if i == 0:
+            cmd = tmp
+        elif optnl:
+            opt_args += tmp
+        else:
+            args.append(tmp)
+        i += 1
+        tmp = ""
+    for ch in ui:
+        if ch == '\'':
+            if sep == '\'':
+                sep = None
+                done_split()
+                continue
+            elif sep == None:
+                sep = '\''
+                continue
+        if ch == '"':
+            if sep == '"':
+                sep = None
+                done_split()
+                continue
+            elif sep == None:
+                sep = '"'
+                continue
+        if ch == '-' and j == 0:
+            optnl = True
+            continue
+        if ch == ' ':
+            if sep == None:
+                done_split()
+                continue
+        tmp += sep
+
 while True:
-    ui = input("$ ").split(" ")
-    cmd = ui[0]
-    args = ui[1:]
-    
+    cmd, args, optl_args = split_args(input("$ "))
