6b0d61458b3eab5d116454bbf69d9603c842d9a6
Sa'ar <geger40.1@gmail.com>
2018-08-28 10:14:35 -0700

    Update winux.py

--- winux.py
+++ winux.py
@@ -1,5 +1,7 @@
 import os
 
+
+
 def split_args(ui):
     i = 0
     j = 0
@@ -8,8 +10,8 @@ def split_args(ui):
     cmd = ""
     args = []
     opt_args = []
-    tmp
-    def done_split:
+    tmp = ""
+    def done_split(i, j, tmp, cmd, opt_args, args, optnl):
         j = 0
         if i == 0:
             cmd = tmp
@@ -19,11 +21,13 @@ def split_args(ui):
             args.append(tmp)
         i += 1
         tmp = ""
+        return (i, j, tmp, cmd, opt_args, args)
     for ch in ui:
         if ch == '\'':
             if sep == '\'':
                 sep = None
-                done_split()
+                i, j, tmp, cmd, opt_args, args = done_split(i, j, tmp, cmd, opt_args, args, optnl)
+                optnl = False
                 continue
             elif sep == None:
                 sep = '\''
@@ -31,7 +35,8 @@ def split_args(ui):
         if ch == '"':
             if sep == '"':
                 sep = None
-                done_split()
+                i, j, tmp, cmd, opt_args, args = done_split(i, j, tmp, cmd, opt_args, args, optnl)
+                optnl = False
                 continue
             elif sep == None:
                 sep = '"'
@@ -41,9 +46,26 @@ def split_args(ui):
             continue
         if ch == ' ':
             if sep == None:
-                done_split()
+                i, j, tmp, cmd, opt_args, args = done_split(i, j, tmp, cmd, opt_args, args, optnl)
+                optnl = False
                 continue
-        tmp += sep
+        tmp += ch
+        j += 1
+    i, j, tmp, cmd, opt_args, args = done_split(i, j, tmp, cmd, opt_args, args, optnl)
+    return (cmd, args, opt_args)
+
+class function:
+    def __init__(self, alwd_args, alwd_optl_args, args, optl_args):
+        if len(args) != alwd_args and alwd_args != -1:
+            raise ValueError(f"{self.__class__.__name__} only accepts {alwd_args} arguments")
+
+class echo(function):
+    def __init__(self, args, optl_args):
+        super(echo, self).__init__(-1, [], args, optl_args)
+        print(" ".join(args))
+
+cmds = {"echo": echo}
 
 while True:
     cmd, args, optl_args = split_args(input("$ "))
+    cmds[cmd](args, optl_args)
